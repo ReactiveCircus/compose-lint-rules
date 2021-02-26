7c32f12645b29a35dde7cd5f8fcbca5291a1e7e2
Yang <reactivecircus@gmail.com>
2021-02-26 13:21:00 +1100

    AGP 7.0.0-alpha08. Compose 1.0.0-beta01, add activity-compose.

--- sample/src/main/java/io/github/reactivecircus/composelint/MainActivity.kt
+++ sample/src/main/java/io/github/reactivecircus/composelint/MainActivity.kt
@@ -3,14 +3,14 @@
 package io.github.reactivecircus.composelint
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
